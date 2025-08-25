9d2f7b24ded27a40aa83a52f111f0eb98e6bab26
Regacoderyangkeren <aryarega2811@gmail.com>
2025-08-25 20:56:09 +0700

    Materi Backend- Syntax Dasar

---
+++ Syntax_dasar/echo.php
@@ -0,0 +1,14 @@
+
+<?php
+// echo, Digunakan untuk menampilkan teks, angka, atau HTML ke browser. Bisa menampilkan lebih dari satu nilai dengan koma atau titik
+echo "Selamat datang di website saya!";
+echo "<br>"; // ganti baris
+echo "Belajar PHP itu menyenangkan.";
+?>
+
+
+<?php
+// menggunakan echo untuk menampilkan teks dengan HTML tag
+echo "<h2>Judul Artikel</h2>";
+echo "<p>Ini paragraf yang ditulis menggunakan PHP.</p>";
+?>
---
+++ Syntax_dasar/testing.php
@@ -0,0 +1,38 @@
+<!DOCTYPE html>
+<html lang ="en">
+    <head>
+        <link rel="stylesheet" href="style.css">
+    </head>
+
+    <body>
+        <h1>Halo</h1>
+    </body>
+</html>
+
+<?php
+    echo "Hello World!";
+?>
+
+<?php
+    $nama = "John Doe";
+    $umur = 25;
+    echo "Halo, $nama! Umur Anda adalah $umur tahun.";
+    // atau
+    echo "Halo, " . $nama . "! Umur Anda adalah " . $umur . " tahun.";
+?>
+
+<?php
+    $a = 5;
+    $b = 10;
+
+    echo $a + $b;
+?>
+
+<?php
+    $teks = "ini teks";
+    $angka = 100;
+    $float = 1.1;
+    $boolean_true = true;
+    $boolean_false = false;
+    $array = ["ini", "array"];
+?>
